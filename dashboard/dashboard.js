ce8b291e6c6d088b637c7b113ace71caf97efca7
funbililian <funbililian@gmail.com>
2026-06-12 23:47:13 +0100

    Change API endpoint to fetch user profile

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -14,7 +14,7 @@ let user = null;
 
 async function loadUserProfile() {
     try {
-        const res = await fetch(`${API_BASE}/auth/me`, {
+        const res = await fetch(`${API_BASE}/auth/get_profile`, {
             method: "GET",
             credentials: "include"
         });
